2d4e0368c7b7a1442a4a4771e3f776960ce0c04c
mwangi-njuguna <mwanginjuguna59@gmail.com>
2018-02-25 13:09:46 +0300

    Added conditonal expressions in the business logic

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 function pingPong(num) {
   var myArray = [];
-
+  for (var a = 1; a <= num; a++) {
+    if (a % 15 === 0) {
+      myArray.push("PING PONG");
+    } else if (a % 5 === 0) {
+      myArray.push("PONG");
+    } else if (a % 3 === 0) {
+      myArray.push("PING");
+    } else {
+      myArray.push(a);
+    }
+  };
   return myArray;
 };
